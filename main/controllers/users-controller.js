699102ff2c5628168872b68cd77e7d68d643dc8b
Samuel Ndara <samuelndara@Samuels-MacBook-Pro.local>
2019-08-02 14:03:25 +0300

    Change promises syntax to async await in UsersController

--- main/controllers/users-controller.js
+++ main/controllers/users-controller.js
@@ -74,36 +74,61 @@ class UsersController {
         //todo: return model info to client alongside token on login
         let authUser = request.body.model;
 
-        return User.findByCriteria({email: authUser.email, id: authUser.id})
-            .then(user => {
+        return UsersController.logoutAUser(authUser.email, authUser.id)
+            .then(result=> response.status(result.status).send(result.data))
+            .catch(error=> response.status(400).send(error));
 
-                if (!user) {
-                    return response.status(404).json(user);
-                }
+    }
 
-                AccessToken.update({revoked: true}, {userID: authUser.id, revoked: false})
-                    .then(token => {
+    async static logoutAUser(email, id){
 
-                        //todo: add response to send
-                        if (!token) {
+        try{
 
-                            let data = {
-                                message: "Token not found"
-                            };
+            let result ={
+                status:205,
+                data:null
+            };
+    
+            //Get the user
+            let user = await User.findByCriteria({email, id})
+    
+            if (!user) {
+                return {
+                    status:404,
+                    data:{
+                        message:`User with ID: ${id} was not found`
+                    }
+                };
+            }
+    
+            //Update the access token
+            let accessToken =  await AccessToken.update(
+                {revoked: true},
+                {userID: id, revoked: false}
+            )
+    
+            if (!accessToken) {
+                return {
+                    status:404,
+                    data:{
+                        message:`Access token for user with ID: ${id} was not found`
+                    }
+                };
+            }
+    
+            //Update the refresh token
+            let refreshToken = RefreshToken.update(
+                {revoked: true},
+                {userID: id, revoked: false}
+            );
+                 
+            console.log(refreshToken);
 
-                            return response.status(404).send(data);
+            return result;
 
-                        }
-
-                        RefreshToken.update({revoked: true}, {userID: authUser.id, revoked: false})
-                            .then(refreshToken => console.log(refreshToken))
-                            .catch(error => console.log(error));
-
-                    })
-                    .catch(error => response.status(400).send(error));
-
-            })
-            .catch(error => response.status(400).send(error));
+        } catch(error){
+            throw new Error(error)
+        }
 
     }
 
